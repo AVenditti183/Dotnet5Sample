d5e6fcaeeea0e18c705a5dcf858f5ac6a9069b2e
AVenditti183 <vasoft83@gmail.com>
2020-11-26 10:54:04 +0100

    fix

--- Server/Startup.cs
+++ Server/Startup.cs
@@ -64,6 +64,7 @@ namespace dotnet5sample.Server
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+            app.UseSwagger();
             app.UseSwaggerUI( c => c.SwaggerEndpoint("/swagger/v1/swagger.json","WebApp1 v1"));
             
             app.UseHttpsRedirection();
